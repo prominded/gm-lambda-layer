91a2d1b4e23ea903d7a5c0b9e84e5e10fb22a1a2
Emmanuel Ogedegbe (dolason) <emmaogedegbe@gmail.com>
2025-06-04 10:30:38 -0500

    upgrade gs to 10.05.01

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 #export GM_VERSION=1.3.31
 #docker build --build-arg GM_VERSION -t gm-lambda-layer .
+#docker run --rm - it gm-lambda-layer cat 
+#docker run --rm -it --name gs-container gs100501-layer /bin/bash
 
-docker build -t gm-lambda-layer .
+docker build -t gm-lambda-layer .   #dockerfile >> Dockerfile
 docker run --rm gm-lambda-layer cat /tmp/gm-1.3.31.zip > ./graphicsmagick.zip
+
+
+docker build -t gs100501-layer .    #dockile >> Dockerfile_gs
+docker run --rm gs100501-layer cat /tmp/gm-1.3.31.zip > ./graphicsmagick.zip
